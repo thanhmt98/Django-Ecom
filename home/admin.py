1a64cfb4f786add8f3b08dbf21d1091f40a5abf6
thanhmt98 <brianmasgt@gmail.com>
2020-09-04 12:53:54 +0700

    first commit

---
+++ home/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+
+# Register your models here.
+from home.models import *
+
+
+admin.site.site_header = 'MG Store DashBoard'
+class SettingtAdmin(admin.ModelAdmin):
+    list_display = ['title','company', 'update_at','status']
+
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ['name','subject', 'update_at','status']
+    readonly_fields =('name','subject','email','message','ip')
+    list_filter = ['status']
+
+
+
+admin.site.register(Setting,SettingtAdmin)
+admin.site.register(ContactMessage,ContactMessageAdmin)
+
+
